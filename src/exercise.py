36bf63cb0ad29cd4d0fc9cdeafd621dd83ba9b2f
Captain C1iff <l.ashdown314@gmx.com>
2020-09-21 09:17:47 +0000

    Works now

--- src/exercise.py
+++ src/exercise.py
@@ -1,8 +1,8 @@
 def main():
     input1 = input("Greetings, how are you doing?")
-    if input1 = "":
+    if input1 != "":
       input2 = input("Oh, how interesting. Tell me more!")
-      if input2 = "":
+      if input2 != "":
         print("Thanks for sharing")
 
 if __name__ == '__main__':
